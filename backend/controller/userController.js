18df76cc5114251e5052a06b8f9812a69d811dff
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-26 14:05:21 -0700

    initial code setup

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,8 @@
+import asyncHandler from 'express-async-handler'
 // @desc Auth user/set token
 // @route POST /api/users/login
 // @access Public
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({message :'User Authenticated'})
-}
+})
 export { authUser }
