56e6d2edc5d1ca06c47b16ada64db6c113a4ef74
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-27 05:34:42 -0700

    add two middlewares to parse the incoming request data

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 connectDB()
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use('/api/users',userRoutes)
 app.get('/',(req,res)=>{
     res.send('Server is ready')
