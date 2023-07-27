e0697fc61003f6da9fba6126bf706c6abc55d986
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-27 05:11:59 -0700

    add routes for updating user profile, registering users and  logout user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import { authUser } from '../controller/userController.js'
+import { authUser,registerUser,logoutUser,getUserProfile,updateUserProfile } from '../controller/userController.js'
 
+router.post('/',registerUser)
 router.post ('/auth',authUser)
+router.post('/logout',logoutUser)
+router.get('/profile',getUserProfile)
+router.put('/profile',updateUserProfile)
 
 export default router
