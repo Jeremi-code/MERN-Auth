849b24de088fb165d98dcd7466ce21243cdaee51
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-29 11:36:04 -0700

    add middleware to the get and put routes of /profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 const router = express.Router()
+import {protect} from "../middleware/authMiddleware.js"
 import { authUser,registerUser,logoutUser,getUserProfile,updateUserProfile } from '../controller/userController.js'
 
 router.post('/',registerUser)
 router.post ('/auth',authUser)
 router.post('/logout',logoutUser)
-router.get('/profile',getUserProfile)
-router.put('/profile',updateUserProfile)
+router.get('/profile',protect,getUserProfile)
+router.put('/profile',protect,updateUserProfile)
 
 export default router
