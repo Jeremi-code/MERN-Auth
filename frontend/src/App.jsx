996f23018203762cc08f091eb60edcdd144fe6e2
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-31 12:15:00 -0700

    wrap the outlet component within container from bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./component/header"
 import { Outlet } from "react-router-dom"
+import { Container } from "react-bootstrap"
 
 const App = () => {
   return(
     <>
       <Header/>
+      <Container>
       <Outlet/>
+      </Container>
     </>
   )
 }
